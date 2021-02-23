eae1868c5196505e19e21e88c762d4b7e591e0b3
Marcos <m.rodrig.felix@gmail.com>
2021-02-22 21:50:49 -0800

    commit tree 00fc6f8469ac603c2e2109a538740ec4755b7932

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+
+
+function map(srcArray, funk){
+  let newArray = []
+  
+  for (let i = 0; i < srcArray.length; i++){
+    newArray.push(funk(srcArray[i]))
+  }
+
+  return newArray
+};
+
+
+
+function reduce(srcArray, funk, startingPoint){
+  let a = (!!startingPoint) ? startingPoint : srcArray[0]
+  let i = (!!startingPoint) ? 0 : 1
+
+  for (; i < srcArray.length; i++){
+    a = funk(srcArray[i], a)
+  }
+
+  return a
+}
